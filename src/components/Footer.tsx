eb9fc632eae54ac9909fe3cb92491e9ae1564fd9
Satwick Pandey <119724322+spsatwickpandey@users.noreply.github.com>
2025-04-21 04:50:39 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -15,7 +15,7 @@ function Footer() {
   };
 
   return (
-    <footer className="relative bg-indigo-900 text-gray-300 pt-20 pb-6">
+    <footer className="relative bg-gradient-to-b from-transparent to-[#0A192F] text-gray-300 pt-20 pb-6">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12">
           {/* Contact Information */}
